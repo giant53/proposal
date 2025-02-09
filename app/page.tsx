74f23fb2bcaf729a1e6816244946b297ec73fc3c
giant53 <giant.dreamhigh@gmail.com>
2025-02-09 16:26:46 +0530

    landing and branding

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import {
   SparklesIcon,
   StarIcon,
 } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -89,15 +90,16 @@ export default function Home() {
       </div>
 
       {/* Main Content */}
-      <div className="relative z-10 container mx-auto px-6 py-24 lg:py-32 text-center">
-        <motion.h1
+      <div className="relative z-10 container mx-auto px-6 py-16 lg:py-32 text-center">
+        <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="text-5xl md:text-6xl lg:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-pink-600 mb-6"
+          className="text-5xl md:text-6xl lg:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-pink-600 mb-6 flex flex-col lg:flex-row-reverse items-center justify-center"
         >
-          myproposal.❤️
-        </motion.h1>
+          <Image src="/wlogo.svg" alt="myproposal." width={300} height={300} />
+          <h1>myproposal.love</h1>
+        </motion.div>
 
         <motion.p
           initial={{ opacity: 0, y: 50 }}
